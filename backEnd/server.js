183e11a9b393bb9699a33eb634d1e129ab69c212
Haafii <hafismuhammedtk@gmail.com>
2023-11-02 23:05:08 +0530

    route set

--- backEnd/server.js
+++ backEnd/server.js
@@ -21,6 +21,10 @@ connection.on('error', ()=>{
     console.log("MongoDB database connection error");
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
 console. log(`Server is running on port: ${port}`);
